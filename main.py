fb55cecf322dd38c39f9679e8dcdb6d9a8481507
WjatscheslawL <w.lapin@gmx.de>
2024-04-12 12:11:43 +0200

    add new const

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame
+import constfile as const
 pygame.init()
+
+screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
+pygame.display.set_caption("Game Tup")
+icon = pygame.image.load("assets")
+
 
 running = True
 
