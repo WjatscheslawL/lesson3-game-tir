992ccee85e63ab8099d54182a476741ee82bf8d3
WjatscheslawL <w.lapin@gmx.de>
2024-04-12 12:57:24 +0200

    add cycle

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ pygame.init()
 
 screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
 pygame.display.set_caption("Game Tup")
-icon = pygame.image.load("images/icon.png")
+icon = pygame.image.load("image/sniper-team.jpg")
 pygame.display.set_icon(icon)
 
 # set target image
-target_img = pygame.image.load("images/zscheibe.png")
+target_img = pygame.image.load("image/zscheibe.jpg")
 target_width = 100
 target_height = 100
 
@@ -22,8 +22,17 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 running = True
 
 while running:
-    pass
-
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y+target_height:
+                target_x = random.randint(0, const.SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, const.SCREEN_HEIGHT - target_height)
+    screen .blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 # eof game
 pygame.quit()
